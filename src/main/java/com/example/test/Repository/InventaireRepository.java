c1e6717821021b7a2fb8b37f1249be32be342ac5
sana <sanabenharb19@gmail.com>
2022-05-23 20:35:25 +0200

    backend1

--- src/main/java/com/example/test/Repository/InventaireRepository.java
+++ src/main/java/com/example/test/Repository/InventaireRepository.java
@@ -1,13 +1,20 @@
 package com.example.test.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.test.Modeles.Inventaire;
+
 
 
 public interface InventaireRepository extends JpaRepository<Inventaire, Integer> {
 	Inventaire findById( int inventaireid);
 	void deleteById(int inventaireid);
 
+	@Query(value = "SELECT * FROM inventaire WHERE user_id = :userid", nativeQuery = true)
+    public List<Inventaire> findBygetuser(@Param("userid") Integer userid);
 
 }
